51f55b653d1ec74d4d883e9791e010a313bc2dcc
Gunawan Saleh <gunawansaleh04@gmail.com>
2020-03-22 18:13:53 +0700

    add error handling for currency category

--- lib/unit_converter.dart
+++ lib/unit_converter.dart
@@ -33,6 +33,8 @@ class _UnitConverterState extends State<UnitConverter> {
   String _convertedValue = '';
   List<DropdownMenuItem> _unitMenuItems;
   bool _showValidationError = false;
+  final _inputKey = GlobalKey(debugLabel: "inputText");
+   bool _showErrorUI = false;
 
   @override
   void initState() {
@@ -106,9 +108,16 @@ class _UnitConverterState extends State<UnitConverter> {
       final conversion = await api.convert(apiCategory["route"],
           _inputValue.toString(), _fromValue.name, _toValue.name);
 
-      setState(() {
-        _convertedValue = _format(conversion);
-      });
+      if (conversion == null) {
+        setState(() {
+          _showErrorUI = true;
+        });
+      } else {
+        setState(() {
+          _showErrorUI = false;
+          _convertedValue = _format(conversion);
+        });
+      }
     } else {
       setState(() {
         _convertedValue = _format(
@@ -198,6 +207,37 @@ class _UnitConverterState extends State<UnitConverter> {
 
   @override
   Widget build(BuildContext context) {
+    if (widget.category.units == null || (widget.category.name == apiCategory["name"] && _showErrorUI)) {
+      return SingleChildScrollView(
+        child: Container(
+          margin: _padding,
+          padding: _padding,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            color: widget.category.color["error"],
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.error_outline,
+                size: 180,
+                color: Colors.white,
+              ),
+              Text(
+                "There is internet connection problem!",
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.headline.copyWith(
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+
     final input = Padding(
       padding: _padding,
       child: Column(
